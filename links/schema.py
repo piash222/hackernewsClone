14891e52e5a0d1cffd0e631a387816a3530cd25f
Shihab Uddin Piash <piash222@gmail.com>
2020-01-29 18:26:31 +0600

    creating mutation with graphql

--- links/schema.py
+++ links/schema.py
@@ -14,4 +14,34 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
-    
+
+
+# define a mutation class. Right after, you define the output of the
+# mutation, the data the server can send back to the client
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # Defines the data you can send to the server
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # It creates a link in the database using the data send by the uesr through url and
+    # description parameters. After the server returns the Createlink class
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description
+        )
+
+
+# creates a mutation class with a field to be resolved, which points to our mutation
+# defined before
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
